38f8034ea65468a933e6d322e980eb52de5f1150
knoc5215 <knoc5215@naver.com>
2020-12-25 14:38:59 +0900

    스프링 시큐리티 1부: Starter-Security - Initial commit

---
+++ src/main/java/me/jumen/demospringbootsecurity/WebSecurityConfig.java
@@ -0,0 +1,8 @@
+package me.jumen.demospringbootsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+}
---
+++ src/test/java/me/jumen/demospringbootsecurity/DemoSpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jumen.demospringbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
